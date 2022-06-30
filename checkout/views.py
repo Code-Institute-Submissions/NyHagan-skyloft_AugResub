55617d6b15f4d1abde5ee3cffc3be7cd5d7ce21b
NyHagan <nanaohagan@gmail.com>
2022-06-30 09:36:12 +0000

    add stripe payment success javascripts function

--- checkout/views.py
+++ checkout/views.py
@@ -32,14 +32,13 @@ def checkout(request):
         currency=settings.STRIPE_CURRENCY,
         )
 
-    print(intent)
-
     order_form = OrderForm()
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51LG7SqLghrfteSL6YLYtFQS5mOrRVaYpRYFWzE2yJ0hRLeYbkb7NJa3F06kSRJCBozoUbtzw8hmvTKQ1WsusroO700Ex341EXr',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
