9bd8721a6c25c119f426cc533baa2408b128ac91
NyHagan <nanaohagan@gmail.com>
2022-07-31 16:12:07 +0000

    add login required

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
+from django.contrib.auth.decorators import login_required
 from . import views
 from .webhooks import webhook
 
 urlpatterns = [
-    path('', views.checkout, name='checkout'),
+    path('', login_required(views.checkout), name='checkout'),
     path('checkout_success/<order_number>',
          views.checkout_success, name='checkout_success'),
     path('cache_checkout_data/', views.cache_checkout_data,
          name='cache_checkout_data'),
     path('wh/', webhook, name='webhook'),
 ]
+
+
